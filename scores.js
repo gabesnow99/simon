3c73648d0872422e07c94a86b24d657900e1493e
gabesnow99 <gabesnow99@penguin.lxd>
2023-03-06 22:29:37 -0700

    began scores.js and added assets file

---
+++ scores.js
@@ -0,0 +1,17 @@
+function loadScores() {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+        scores = JSON.parse(scoresText);
+    }
+
+    const tableBodyEl = document.querySelector('#scores');
+
+    if (scores.length) {
+        for (const [i, score] of scores.entries()) {
+            const positionTdEl = document.createElement('td');
+        }
+    }
+}
+
+loadScores();
